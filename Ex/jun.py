291f98452a96f2dc09cfef028929a2cdb0a84483
brwnry <119073714+brwnry@users.noreply.github.com>
2024-07-01 17:59:44 +0000

    exercício 39

--- Ex/jun.py
+++ Ex/jun.py
@@ -1,9 +1,12 @@
-nota_1 = float(input('Digite a nota do primeiro aluno: '))
-nota_2 = float(input('Digite a nota do segundo aluno: '))
-nota_3 = float(input('Digite a nota do terceiro aluno: '))
+# o comando for é uma estrutura de repetição, que permite que você execute um bloco de código várias vezes.
+#
+for i in range(10): # range(10) = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  print(i) # imprima i
 
-media = (nota_1 + nota_2 + nota_3) / 3
-nota_alta = max(nota_1, nota_2, nota_3)
+# Exemplos:
+# Pegar um número variável de nomes e exibí-los em seguida:
 
-print (f'A média da turma é {media}')
-print(f'A maior nota da turma é {nota_alta}') 
+qt_nomes = int(input('Quantos nomes você quer? ')) # int
+for i in range(qt_nomes): # range(qt_nomes) = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  nome = input('Digite um nome: ') # str
+  print('nome #  {}' .format(i+1), nome)
