c960cb9e3e632ef2123dd7d80bb375f861671aff
CapnJackrabbit <acemustang@live.com>
2024-07-17 17:28:41 -0300

    exercicio resolvido

--- Udemy/Aula 054 - Exercicio.py
+++ Udemy/Aula 054 - Exercicio.py
@@ -7,3 +7,46 @@
 # Tela de entrada:
 # Selecione uma das opções abaixo:
 # [i]nserir [a]pagar [l]istar
+
+def insere():
+    item = input('Nome do item a inserir: ')
+    lista_compras.append(item)
+    lista()
+
+def apaga():
+    item = input('Nome do item a ser excluido: ')
+    if item not in lista_compras:
+        print('*** Item nao existe na lista ***')
+    else:
+        lista_compras.remove(item)
+        if item not in lista_compras:
+            print('*** Item removido com sucesso! ***')
+            print()
+
+def lista():
+    if len(lista_compras) == 0:
+        print('*** A lista esta vazia ***\n')
+    else:
+        print()
+        print('*** Lista de produtos cadastrados ate o momento: ***\n')
+        for produto in lista_compras:
+            print(lista_compras.index(produto)+1, '-', produto + ', ')
+    print()
+
+
+lista_compras = []
+
+
+while True:
+    opcao = input('Selecione uma das opções abaixo:\n[i]nserir  [a]pagar  [l]istar [s]air  ').lower()
+
+    if opcao == 'i':
+        insere()
+    elif opcao == 'a':
+        apaga()
+    elif opcao == 'l':
+        lista()
+    elif opcao == 's':
+        break
+    else:
+        print('Opcao invalida!')
