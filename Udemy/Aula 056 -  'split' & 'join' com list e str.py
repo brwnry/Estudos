7203c8bbfbec2c4f1b2de946cfedc32d6f4862d2
brwnry <roybrun@outlook.com>
2024-09-08 17:14:54 -0300

    a

--- Udemy/Aula 056 -  'split' & 'join' com list e str.py
+++ Udemy/Aula 056 -  'split' & 'join' com list e str.py
@@ -5,19 +5,19 @@
 
 # Exemplo split:
 
-frase = 'Olha só que, coisa interessante' # str
-lista_palavras = frase.split() # separa a string em uma lista
-print(lista_palavras) # ['Olha', 'só', 'que,', 'coisa', 'interessante']
+frase = 'Olha só que, coisa interessante'                                                       # str
+lista_palavras = frase.split()                                                                           # separa a string em uma lista
+print(lista_palavras)                                                                                       # ['Olha', 'só', 'que,', 'coisa', 'interessante']
 
-frase = 'Olha só que, coisa interessante' # str
-lista_palavras = frase.split(,) # separa a string em uma lista usando o separador ','
-print(lista_palavras) # ['Olha', 'só', 'que,', 'coisa', 'interessante']
+frase = 'Olha só que, coisa interessante'                                                       # str
+lista_palavras = frase.split(,)                                                                          # separa a string em uma lista usando o separador ','
+print(lista_palavras)                                                                                       # ['Olha', 'só', 'que,', 'coisa', 'interessante']
 
 # Exemplo join:
 
-frases_unidas = ''.join(lista_palavras) # junta a lista em uma string
-print(frases_unidas) # Olha só que, coisa interessante
+frases_unidas = ''.join(lista_palavras)                                                             # junta a lista em uma string
+print(frases_unidas)                                                                                        # Olha só que, coisa interessante
 
 frases_unidas = '-'.join(lista_palavras)
-print(frases_unidas) # Olha-só-que-coisa-interessante
+print(frases_unidas)                                                                                        # Olha-só-que-coisa-interessante
 
